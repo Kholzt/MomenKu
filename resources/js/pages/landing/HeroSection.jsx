f3c25dcc2716f2bef27772bf0e5724683c2a4cb2
Kholzt <99956928+Kholzt@users.noreply.github.com>
2024-12-27 10:07:15 +0700

    Update HeroSection.jsx

--- resources/js/pages/landing/HeroSection.jsx
+++ resources/js/pages/landing/HeroSection.jsx
@@ -20,8 +20,8 @@ const HeroSection = () => {
                             <i className="fa fa-plus-circle"></i> Buat Undangan
                         </Button>
                         <Button type="link" variant={"secondary-outline"}>
-                            <i className="fa fa-cirle-play"></i> Tutorial
-                            Undangan
+                            <i class="fa-regular fa-circle-play text-[--secondary-color]"></i>{" "}
+                            Tutorial Undangan
                         </Button>
                     </div>
                 </div>
